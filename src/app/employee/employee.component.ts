1f3f165507cf1b58eea90bf4076dd295f33fc806
Khitish Mangal <khitish.mangal@netprophetsglobal.com>
2022-12-14 18:42:57 +0530

    Employee CRUD Application

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
 import { EmployeeService } from '../service/employee.service';
 
 @Component({
@@ -19,33 +19,53 @@ export class EmployeeComponent implements OnInit {
     confirmpassword: new FormControl(''),
     check: new FormControl(''),
   });
-  jsondata: any;
 
-  constructor(private fb: FormBuilder, private service: EmployeeService) {}
+  constructor(private service: EmployeeService,private fb:FormBuilder) {}
 
   // postdata
   onSubmit() {
-    this.jsondata = JSON.stringify(this.employeeForm.value);
-    console.log(this.jsondata);
-    this.service.postEmployee(this.employeeForm.value).subscribe((res) => {});
+    this.service.postEmployee(this.employeeForm.value).subscribe((res) => {
+      alert("Data added successfully!");
+      this.employeeForm.reset();
+    });
   }
-
-  // column = [
-  //   'Full Name',
-  //   'Phone Number',
-  //   'Email	DOB',
-  //   'Designation',
-  //   'Gender',
-  //   'Password',
-  //   'Actions',
-  // ];
-
-  index: any = ['', '', '', '', '', '', ''];
+  
+  updateData(data:any){
+    const btn3= document.querySelectorAll('.edit');
+    const modal = document.querySelector('.card') as HTMLDivElement;
+    const background = document.querySelector(
+      '.background-modal'
+    ) as HTMLDivElement;
+    for(let i=0;i<btn3.length;i++)
+    {
+      btn3[i].addEventListener('click',()=>{
+        modal.classList.remove('d-none');
+        background.classList.remove('d-none');
+        this.service.getEmployee().subscribe((res: any) => {
+          this.employeeData = res;
+        });
+        console.log(this.employeeData[i]);
+        // return this.employeeData[i];
+        if(this.employeeData[i]){
+          this.employeeForm.controls['fullname'].setValue(this.employeeData[i].fullname)
+          this.employeeForm.controls['phone'].setValue(this.employeeData[i].phone)
+          this.employeeForm.controls['email'].setValue(this.employeeData[i].email)
+          this.employeeForm.controls['dob'].setValue(this.employeeData[i].dob)
+          this.employeeForm.controls['designation'].setValue(this.employeeData[i].designation)
+          this.employeeForm.controls['gender'].setValue(this.employeeData[i].gender)
+          this.employeeForm.controls['password'].setValue(this.employeeData[i].password)
+          this.employeeForm.controls['confirmpassword'].setValue(this.employeeData[i].confirmpassword)
+        }
+      })
+    }
+  }
+  employeeData: any = ['', '', '', '', '', '', ''];
 
   ngOnInit(): void {
     const btn = document.querySelector('.btn') as HTMLButtonElement;
     const btn1 = document.querySelector('.close-modal') as HTMLButtonElement;
     const btn2 = document.querySelector('.submit-button') as HTMLButtonElement;
+    // const btn3 = document.querySelector('.edit') as HTMLButtonElement;
     const modal = document.querySelector('.card') as HTMLDivElement;
     const background = document.querySelector(
       '.background-modal'
@@ -62,12 +82,15 @@ export class EmployeeComponent implements OnInit {
 
     btn.addEventListener('click', openModal);
     btn1.addEventListener('click', closeModal);
-    btn2.addEventListener('click', closeModal);
     background.addEventListener('click', closeModal);
-
+    
+    // getdata
     this.service.getEmployee().subscribe((res) => {
-      this.index = res;
-      console.log(this.index[0]);
+      this.employeeData = res;
+      // console.log(this.employeeData[0]);
+      btn2.addEventListener('click', closeModal);
     });
+
+    
   }
 }
