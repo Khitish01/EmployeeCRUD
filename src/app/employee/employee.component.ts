edefa98b5b8a10e6c50b3a0877d7261abec63d79
Khitish Mangal <khitish.mangal@netprophetsglobal.com>
2022-12-14 12:09:43 +0530

    Employee Data

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,10 +8,7 @@ import { EmployeeService } from '../service/employee.service';
   styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
-
-  
-  
-  employeeForm= new FormGroup({
+  employeeForm = new FormGroup({
     fullname: new FormControl(''),
     phone: new FormControl(''),
     email: new FormControl(''),
@@ -20,28 +17,39 @@ export class EmployeeComponent implements OnInit {
     gender: new FormControl(''),
     password: new FormControl(''),
     confirmpassword: new FormControl(''),
-    check: new FormControl('')
+    check: new FormControl(''),
   });
   jsondata: any;
-  
-  constructor(private fb:FormBuilder,private service:EmployeeService) {  }
 
+  constructor(private fb: FormBuilder, private service: EmployeeService) {}
 
-  
-  onSubmit(){
-    this.jsondata=JSON.stringify(this.employeeForm.value);
+  // postdata
+  onSubmit() {
+    this.jsondata = JSON.stringify(this.employeeForm.value);
     console.log(this.jsondata);
-    this.service.postEmployee(this.employeeForm.value).subscribe( (res) => {
-
-    });
+    this.service.postEmployee(this.employeeForm.value).subscribe((res) => {});
   }
 
-  
+  // column = [
+  //   'Full Name',
+  //   'Phone Number',
+  //   'Email	DOB',
+  //   'Designation',
+  //   'Gender',
+  //   'Password',
+  //   'Actions',
+  // ];
+
+  index: any = ['', '', '', '', '', '', ''];
+
   ngOnInit(): void {
     const btn = document.querySelector('.btn') as HTMLButtonElement;
     const btn1 = document.querySelector('.close-modal') as HTMLButtonElement;
+    const btn2 = document.querySelector('.submit-button') as HTMLButtonElement;
     const modal = document.querySelector('.card') as HTMLDivElement;
-    const background = document.querySelector('.background-modal') as HTMLDivElement;
+    const background = document.querySelector(
+      '.background-modal'
+    ) as HTMLDivElement;
 
     const openModal = () => {
       modal.classList.remove('d-none');
@@ -53,44 +61,13 @@ export class EmployeeComponent implements OnInit {
     };
 
     btn.addEventListener('click', openModal);
-    btn1.addEventListener('click',closeModal);
-    background.addEventListener('click',closeModal);
+    btn1.addEventListener('click', closeModal);
+    btn2.addEventListener('click', closeModal);
+    background.addEventListener('click', closeModal);
+
+    this.service.getEmployee().subscribe((res) => {
+      this.index = res;
+      console.log(this.index[0]);
+    });
   }
-
-
-
-
-
-
-
-
-
-
-  // public get FullName(): FormControl {
-  //   return this.employeeForm.get('fullname') as FormControl;
-  // }
-  // public get PhoneNumber(): FormControl {
-  //   return this.employeeForm.get('phone') as FormControl;
-  // }
-  // public get Email(): FormControl {
-  //   return this.employeeForm.get('email') as FormControl;
-  // }
-  // public get DOB(): FormControl {
-  //   return this.employeeForm.get('dob') as FormControl;
-  // }
-  // public get Designation(): FormControl {
-  //   return this.employeeForm.get('designation') as FormControl;
-  // }
-  // public get gender(): FormControl {
-  //   return this.employeeForm.get('gender') as FormControl;
-  // }
-  // public get Password(): FormControl {
-  //   return this.employeeForm.get('password') as FormControl;
-  // }
-  // public get ConfirmPassword(): FormControl {
-  //   return this.employeeForm.get('confirmpassword') as FormControl;
-  // }
-  // public get Checkbox(): FormControl {
-  //   return this.employeeForm.get('salary') as FormControl;
-  // }
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -5,10 +5,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EmployeeService {
-  baseUrl= 'http://localhost:3000/posts';
+  baseUrl:string= 'http://localhost:3000/posts';
   constructor(private http:HttpClient) { }
 
   postEmployee(data:any) {
     return this.http.post(this.baseUrl, data);
+  }
+
+  getEmployee(){
+    return this.http.get(this.baseUrl);
   }
 }
